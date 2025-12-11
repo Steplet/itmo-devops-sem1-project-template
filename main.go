5f6b45c7b7efeb7ae5d230ff05f3ee5205a8a975
Артём Стеберг <asteberg@list.ru>
2025-12-11 18:41:03 +0300

    add entry point

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"project_sem/internal/db"
+	"project_sem/internal/handlers"
+)
+
+func main() {
+	dataBase, err := db.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("POST /api/v0/prices", handlers.PostPrice(dataBase))
+	http.HandleFunc("GET /api/v0/prices", handlers.GetPrice(dataBase))
+
+	log.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
